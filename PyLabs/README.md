4c62aecf78d07d49f698891724d78cadb5d011c1
Daniel Wolkow <volkov12@rambler.ru>
2017-10-31 21:35:05 +0300

    delete many files after divide repo

--- PyLabs/README.md
+++
@@ -1,3 +0,0 @@
-# Description
-
-Here is labs for students GUAP University 2017 Fall -- Python Programming Technology and Statistic Models
--- stepic_sols/yandex_cpp/marrstar.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    ushort n;
-    std::cin >> n;
-    for (ushort i = 0; i < n; ++i) 
-        for (ushort j = 0; j < n; ++j)
-            std::cout << ((i == j || i == n/2 || j == n/2 || i == n - j - 1) ? "* " : ". ") << (j == n - 1 ? "\n" : "");
-}
